7a47c533e6ef31277c7947e8c7e2f0694f3df4f2
Herman Lundby-Holen <hermanluho@gmail.com>
2025-08-27 16:45:10 +0200

    Stylized landing page and created a global layout file.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<style>
+	:global {
+		section {
+			font-family: sans-serif;
+			background-color: azure;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
-<!-- TODO: Create a check to see if user is authenticated, if they are, navigate to dashboard -->
 <script lang="ts">
 	import { isSession } from '$lib/supabase';
-	import { redirect } from '@sveltejs/kit';
 	import { onMount } from 'svelte';
 
 	let sessionExists: boolean = false;
@@ -16,5 +14,31 @@
 </script>
 
 {#if !sessionExists}
-	<a href="/login">Log in</a>
+	<section id="landing-screen">
+		<h1>Create collections, add images, <br /> and share with anyone you want!</h1>
+		<p>A platform where likes and comments don't exist.</p>
+		<a href="/login" id="log-in-btn">Log in</a>
+	</section>
 {/if}
+
+<style>
+	#landing-screen {
+		height: 100dvh;
+		display: grid;
+		place-content: center;
+	}
+
+	#log-in-btn {
+		background-color: antiquewhite;
+		color: black;
+		text-decoration: none;
+		text-align: center;
+		border: 1px solid black;
+		border-radius: 8px;
+		padding: 1rem;
+		cursor: pointer;
+	}
+	#log-in-btn:hover {
+		background-color: rgb(227, 203, 173);
+	}
+</style>
