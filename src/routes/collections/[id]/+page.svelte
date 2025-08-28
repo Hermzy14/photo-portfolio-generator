6c979983286150269e5c25eaeb6720d390881ce0
Herman Lundby-Holen <hermanluho@gmail.com>
2025-08-28 20:59:55 +0200

    Added a display of the images in the collection on the edit screen.

--- src/routes/collections/[id]/+page.svelte
+++ src/routes/collections/[id]/+page.svelte
@@ -21,6 +21,7 @@
 	onMount(async () => {
 		try {
 			data = await getCollectionById(collectionId);
+			console.log(data);
 		} catch (error) {
 			console.error(error);
 			errorMessage = 'An error occured while fetching collection.';
@@ -68,45 +69,59 @@
 	}
 </script>
 
-<section id="collection-edit">
-	<!-- Collection title with an edit button/field -->
-	<div class="collection-edit-section">
-		<h1>Collection Title</h1>
-		<button class="btn">Edit</button>
-	</div>
-
-	<!-- Collection description with an edit button/field -->
-	<div class="collection-edit-section">
-		<p>Collection description goes here.</p>
-		<button class="btn">Edit</button>
-	</div>
-
-	<!-- Update if public or not -->
-	<div class="collection-edit-section">
-		<label>
-			<input type="checkbox" checked />
-			Public Collection
-		</label>
-	</div>
-
-	<!-- Add and remove images -->
-	<div class="collection-edit-section">
-		<input type="file" accept="image/*" multiple onchange={handleImageChange} />
-	</div>
-
-	<a href="/dashboard" class="btn">Save</a>
-	<button onclick={confirmDelete} class="btn" id="delete-btn">Delete collection</button>
-
-	{#if showDeleteConfirm}
-		<div class="modal-backdrop">
-			<div class="modal">
-				<p>Are you sure you want to delete this collection?</p>
-				<button class="btn" onclick={confirmDeleteAction}>Yes, delete</button>
-				<button class="btn" onclick={cancelDelete}>Cancel</button>
-			</div>
+{#if errorMessage}
+	<p class="error">{errorMessage}</p>
+{:else if data}
+	<section id="collection-edit">
+		<!-- Collection title with an edit button/field -->
+		<div class="collection-edit-section">
+			<h1>Collection Title</h1>
+			<button class="btn">Edit</button>
 		</div>
-	{/if}
-</section>
+
+		<!-- Collection description with an edit button/field -->
+		<div class="collection-edit-section">
+			<p>desc</p>
+			<button class="btn">Edit</button>
+		</div>
+
+		<!-- Update if public or not -->
+		<div class="collection-edit-section">
+			<label>
+				<input type="checkbox" checked />
+				Public Collection
+			</label>
+		</div>
+
+		<!-- Add and remove images -->
+		<div class="collection-edit-section">
+			<div id="images-wrapper">
+				{#each data.images as image}
+					<div class="image-container">
+						<img class="collection-images" src={getImageUrl(image.file_path)} alt={image.title} />
+						<button class="btn">Remove</button>
+					</div>
+				{/each}
+			</div>
+			<input type="file" accept="image/*" multiple onchange={handleImageChange} />
+		</div>
+
+		<a href="/dashboard" class="btn">Save</a>
+		<button onclick={confirmDelete} class="btn" id="delete-btn">Delete collection</button>
+
+		{#if showDeleteConfirm}
+			<div class="modal-backdrop">
+				<div class="modal">
+					<p>Are you sure you want to delete this collection?</p>
+					<button class="btn" onclick={confirmDeleteAction}>Yes, delete</button>
+					<button class="btn" onclick={cancelDelete}>Cancel</button>
+				</div>
+			</div>
+		{/if}
+	</section>
+{:else}
+	<p>Loading...</p>
+{/if}
 
 <style>
 	#collection-edit {
@@ -119,15 +134,38 @@
 
 	.collection-edit-section {
 		display: flex;
+		flex-direction: column;
 		gap: 1rem;
 		border: 1px solid black;
 		border-radius: 8px;
 		padding: 1rem;
 	}
 
+	#images-wrapper {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 0.5rem;
+	}
+
+	.image-container {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.collection-images {
+		width: 20dvw;
+		height: 20dvw;
+		object-fit: cover;
+		border: 1px solid black;
+		border-radius: 8px;
+		margin: 1rem 0 0.25rem 0;
+	}
+
 	#delete-btn {
 		background-color: lightcoral;
 	}
+
 	.modal-backdrop {
 		position: fixed;
 		top: 0;
@@ -140,6 +178,7 @@
 		justify-content: center;
 		z-index: 1000;
 	}
+
 	.modal {
 		background: azure;
 		padding: 2rem;
